3e07bb8d9860a8902814bc245be05c68d5dd4936
Aylla Scaglia <aylla@scaglia.com.br>
2026-03-09 11:43:19 -0300

    feat: add database migrations with Flask-Migrate

--- todolist-api/app/__init__.py
+++ todolist-api/app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_migrate import Migrate
 from config import Config
 
+
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -15,6 +18,7 @@ def create_app(test_config=None):
         app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
