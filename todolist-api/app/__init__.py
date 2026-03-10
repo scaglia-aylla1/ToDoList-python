6df63e722c532ed4ea5f8080326173e60fdbd922
Aylla Scaglia <aylla@scaglia.com.br>
2026-03-10 20:28:57 -0300

    fix: add required Flask-Smorest OpenAPI configuration

--- todolist-api/app/__init__.py
+++ todolist-api/app/__init__.py
@@ -6,6 +6,15 @@ from .extensions import db, migrate, api
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    app.config.update({
+        "API_TITLE": "Todo List API",
+        "API_VERSION": "v1",
+        "OPENAPI_VERSION": "3.0.3",
+        "OPENAPI_URL_PREFIX": "/docs",
+        "OPENAPI_SWAGGER_UI_PATH": "/swagger",
+        "OPENAPI_SWAGGER_UI_URL": "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    })
+    
     if test_config:
         app.config.update(test_config)
     else:
